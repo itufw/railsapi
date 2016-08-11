c5275433355aabf551acc810ab84521e93af8d8e
Hinam <mehrahinam@gmail.com>
2016-08-11 17:06:23 +1000

    setup xero api class

---
+++ bigcommerce/lib/xero_connection.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'xeroizer'
+
+class XeroConnection
+
+	def connect
+
+		client = Xeroizer::PrivateApplication.new('YLOXMTHAIZFN3UOE5C2DTG4HHEPKD7', '9FQENZIBR4STELO2PM1PD4ZUM3UIMP', "config/privatekey.pem")
+		return client
+
+	end
+end
