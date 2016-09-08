1f4378c47492b61dd2e35394919818d6e84ed476
Hinam <mehrahinam@gmail.com>
2016-09-08 16:01:11 +1000

    fixed invoices migration

--- bigcommerce/db/migrate/20160908055653_create_xero_invoices.rb
+++ bigcommerce/db/migrate/20160908055653_create_xero_invoices.rb
@@ -2,7 +2,7 @@ class CreateXeroInvoices < ActiveRecord::Migration
   def change
     create_table :xero_invoices, :id => false do |t|
       t.string :xero_invoice_id, limit: 36, primary: true, null: false, index: true
-      t.integer :xero_invoice_number, limit: 8, unsigned: true, index: true
+      t.string :xero_invoice_number, index: true
       t.string :xero_contact_id, limit: 36, null: false, index: true
       t.string :xero_contact_name
       t.decimal :sub_total, :total_tax, :total, :amount_due, :amount_paid, :amount_credited, precision: 8, scale: 2
