be1620cf1fc5d2848069300c83c13f5f56259b93
Hinam <mehrahinam@gmail.com>
2016-09-08 15:48:16 +1000

    total discount column not available in xeroizer gem

--- bigcommerce/lib/clean_data.rb
+++ bigcommerce/lib/clean_data.rb
@@ -6,7 +6,7 @@ class CleanData
 
 	def map_date(date)
 		return_date = "0000-00-00 00:00:00"
-		if date.strip.length != 0
+		if !date.nil? && date.strip.length != 0
 			return_date = date.in_time_zone('Melbourne').strftime("%Y-%m-%d %H:%M:%S")
 		end
 		return return_date
