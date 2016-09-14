41c8efb9f2b5b48bb5ff276506eb69fd30780590
Hinam <mehrahinam@gmail.com>
2016-09-14 17:18:55 +1000

    partial views for order details and order

--- bigcommerce/app/helpers/sales_helper.rb
+++ bigcommerce/app/helpers/sales_helper.rb
@@ -1,15 +1,5 @@
 module SalesHelper
   
-  # def display_staff_totals(active_sales_id, dates_a, sales_totals_h)
-
-  # 	dates_a.each do |d|
-  # 	  staff_key = [active_sales_id, d]
-  #     if sales_totals_h.has_key?(staff_key)
-
-  # 	end
-
-  # end
-
   def sums_dates_h_for_staff(staff_id, sums_dates_staffs_h)
   	filter_by_staff = sums_dates_staffs_h.select {|key, sum| key[0] == staff_id}
 
