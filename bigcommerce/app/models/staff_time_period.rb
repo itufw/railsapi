d48f3b3fdfec9cfa0bc137263c7d1bbe94fc8789
Hinam <mehrahinam@gmail.com>
2016-10-06 11:19:03 +1100

    added a day to staff_time_periods end_date to fix calculations

--- bigcommerce/app/models/staff_time_period.rb
+++ bigcommerce/app/models/staff_time_period.rb
@@ -5,16 +5,16 @@ class StaffTimePeriod < ActiveRecord::Base
 	belongs_to :staff
 
 	def update_this_month(time_period, date_today)
-		StaffTimePeriod.update(time_period.id, {"start_date" => date_today.at_beginning_of_month, "end_date" => date_today})
+		StaffTimePeriod.update(time_period.id, {"start_date" => date_today.at_beginning_of_month, "end_date" => date_today.next_day})
 	end
 
 	def update_last_month(time_period, date_today)
-		StaffTimePeriod.update(time_period.id, start_date: date_today.at_beginning_of_month.last_month, end_date: date_today.at_end_of_month.last_month)
+		StaffTimePeriod.update(time_period.id, start_date: date_today.at_beginning_of_month.last_month, end_date: (date_today.at_end_of_month.last_month).next_day)
 	end
 
 	def update_using_default_start_date(time_period, date_today)
 		set_time_zone = 'Melbourne'
-		StaffTimePeriod.update(time_period.id, start_date: date_today - time_period.start_day, end_date: date_today - time_period.end_day)
+		StaffTimePeriod.update(time_period.id, start_date: date_today - time_period.start_day, end_date: (date_today - time_period.end_day).next_day)
 
 	end
 
