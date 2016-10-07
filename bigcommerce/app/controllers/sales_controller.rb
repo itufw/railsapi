ceb637d2c297aa4299e8fe9d5fb56dc9b1cf2b92
Hinam <mehrahinam@gmail.com>
2016-10-07 11:57:32 +1100

    fixed orders for product

--- bigcommerce/app/controllers/sales_controller.rb
+++ bigcommerce/app/controllers/sales_controller.rb
@@ -144,7 +144,7 @@ class SalesController < ApplicationController
     @statuses = Status.all
 
     @staff, @status, orders_filtered_by_param, @search_text = order_param_filter(params, session[:user_id])
-    orders_filtered_by_product = Order.product_filter(@product_ids).pluck("id")
+    orders_filtered_by_product = Order.product_filter([@product_id]).pluck("id")
     order_ids = orders_filtered_by_param.pluck("id") & orders_filtered_by_product
     @orders = Order.include_customer_staff_status.order_filter_by_ids(order_ids).order_by_id.page(params[:page])
     @staff_nickname = params[:staff_nickname]
