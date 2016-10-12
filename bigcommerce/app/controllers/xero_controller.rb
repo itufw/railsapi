f46b19d7081642163649e5c583b5a09d65bf7bc4
Hinam <mehrahinam@gmail.com>
2016-10-12 15:20:36 +1100

    linked bigc customer with xero contact and displayed outstanding, overdue values in customer view

--- bigcommerce/app/controllers/xero_controller.rb
+++ bigcommerce/app/controllers/xero_controller.rb
@@ -1,7 +1,10 @@
+require 'xero_controller_helper.rb'
 class XeroController < ApplicationController
 
+	include XeroControllerHelper
+	
 	def link_bigc_xero_contact
-		Customer.all.each {|c| Customer.insert_xero_contact_id(c.id, link_skype_id(c.id))}
+		Customer.xero_contact_id_is_null.each {|c| Customer.insert_xero_contact_id(c.id, link_skype_id(c.id))}
 	end
 
 end
--- bigcommerce/app/models/xero_contact.rb
+++ bigcommerce/app/models/xero_contact.rb
@@ -4,7 +4,7 @@ require 'clean_data.rb'
 class XeroContact < ActiveRecord::Base
 
 	include CleanData
-    self.primary_key = 'xero_invoice_id'
+    self.primary_key = 'xero_contact_id'
 
     has_many :xero_invoices
     has_many :xero_payments
@@ -26,11 +26,21 @@ class XeroContact < ActiveRecord::Base
 			skype = remove_apostrophe(c.skype_user_name)
 			updated_date = map_date(c.updated_date_utc)
 
-            unless c.balances.nil?
+            is_supplier = convert_bool(c.is_supplier)
+            is_customer = convert_bool(c.is_customer)
+
+            contact_groups = convert_empty_list(c.contact_groups)
+
+            if c.balances.nil?
+                accounts_receivable_outstanding = 0.00
+                accounts_receivable_overdue = 0.00
+                accounts_payable_outstanding = 0.00
+                accounts_payable_overdue = 0.00
+            else
                 accounts_receivable_outstanding = c.balances.accounts_receivable.outstanding
                 accounts_receivable_overdue = c.balances.accounts_receivable.overdue
-                accounts_payable = c.balances.accounts_payable.outstanding
-                accounts_payable = c.balances.accounts_payable.overdue
+                accounts_payable_outstanding = c.balances.accounts_payable.outstanding
+                accounts_payable_overdue = c.balances.accounts_payable.overdue
             end
 
 			time = Time.now.to_s(:db)
@@ -44,9 +54,9 @@ class XeroContact < ActiveRecord::Base
 	  		VALUES ('#{c.contact_id}', '#{contact_name}', '#{firstname}', '#{lastname}',\
 	  		'#{email}', '#{skype}', '#{c.contact_number}', '#{c.contact_status}',\
 	  		'#{updated_date}', '#{c.account_number}', '#{c.tax_number}', '#{c.bank_account_details}',\
-	  		'#{c.accounts_receivable_tax_type}', '#{c.accounts_payable_tax_type}', '#{c.contact_groups}',\
+	  		'#{c.accounts_receivable_tax_type}', '#{c.accounts_payable_tax_type}', '#{contact_groups}',\
 	  		'#{c.default_currency}', '#{c.purchases_default_account_code}', '#{c.sales_default_account_code}',\
-	  		'#{c.is_supplier}', '#{c.is_customer}', '#{time}', '#{time}', '#{accounts_receivable_outstanding}',\
+	  		'#{is_supplier}', '#{is_customer}', '#{time}', '#{time}', '#{accounts_receivable_outstanding}',\
             '#{accounts_receivable_overdue}', '#{accounts_payable_outstanding}', '#{accounts_payable_overdue}')"
 
 	  		ActiveRecord::Base.connection.execute(sql)
@@ -54,7 +64,16 @@ class XeroContact < ActiveRecord::Base
     end
 
     def self.find_by_skype_id(skype_id)
-    	return where(skype_user_name: skype_id)
+    	return where(skype_user_name: skype_id).first
+    end
+
+    def self.get_accounts_receivable_outstanding(xero_contact_id)
+        return find(xero_contact_id).accounts_receivable_outstanding if xero_contact_id
+
+    end
+
+    def self.get_accounts_receivable_overdue(xero_contact_id)
+        return find(xero_contact_id).accounts_receivable_overdue if xero_contact_id
     end
 
 end
