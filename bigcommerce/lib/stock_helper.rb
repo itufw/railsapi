e31f0a2c38976d1f0b15309c9eb6001ddb13431d
Hinam <mehrahinam@gmail.com>
2016-10-17 17:06:25 +1100

    current quarter function

--- bigcommerce/lib/stock_helper.rb
+++ bigcommerce/lib/stock_helper.rb
@@ -1,11 +1,15 @@
 module StockHelper
 
-	def total_stock_product(product_id)
+	def total_stock_product(product_id, param_total_stock)
 		product_no_vintage_id = Product.no_vintage_id(product_id)
-		similar_products = Product.products_with_same_no_vintage_id(product_no_vintage_id)
-		inventory_sum = 0
-		similar_products.each {|p| inventory_sum += p.inventory}
-		pending_stock_sum = (Product.pending_stock(similar_products.pluck("id"))).values.sum
-		return pending_stock_sum + inventory_sum
+		if product_no_vintage_id.nil?
+			return param_total_stock.to_i
+		else
+			similar_products = Product.products_with_same_no_vintage_id(product_no_vintage_id)
+			inventory_sum = 0
+			similar_products.each {|p| inventory_sum += p.inventory}
+			pending_stock_sum = (Product.pending_stock(similar_products.pluck("id"))).values.sum
+			return pending_stock_sum + inventory_sum
+		end
 	end
 end
