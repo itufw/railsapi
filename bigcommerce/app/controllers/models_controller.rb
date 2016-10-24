6c399b46e450809fe505247438872e2535368426
Hinam <mehrahinam@gmail.com>
2016-10-24 11:20:03 +1100

    merged with master

--- bigcommerce/app/controllers/models_controller.rb
+++ bigcommerce/app/controllers/models_controller.rb
@@ -7,8 +7,6 @@ class ModelsController < ApplicationController
   include ModelsFilter
   
   def orders
-<<<<<<< HEAD
-
     @staffs = Staff.active_sales_staff
     @statuses = Status.all
 
@@ -28,43 +26,6 @@ class ModelsController < ApplicationController
   def customers
     @staffs = Staff.active_sales_staff
 
-    @staff, customers, @search_text = customer_param_filter(params, session[:user_id])
-    @customers = customers.order_by_name.page(params[:page])
-=======
-
-    @staffs = Staff.active_sales_staff
-    @statuses = Status.all
-
-    @staff, @status, orders, @search_text = order_param_filter(params, session[:user_id])
-    @orders = orders.order_by_id.page(params[:page])
-
-    # @staff = results_val[0]
-    # @status = results_val[1]
-    # @orders = results_val[2].order_by_id.page(params[:page])
-    # @search_text = results_val[3]
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
->>>>>>> efafc87de3efa0844d5de64d6f57e3d3288f8a05
-
-    @staff_nickname = params[:staff_nickname]
-  end
-
-  def customers
-    @staffs = Staff.active_sales_staff
-
-<<<<<<< HEAD
-    @countries = ProducerCountry.all
-    @sub_types = ProductSubType.all
-
-    results_val = product_param_filter(params)
-
-    @producer_country = results_val[0]
-    @product_sub_type = results_val[1]
-    @products = results_val[2].order_by_name.page(params[:page])
-    @search_text = results_val[3]
-
-    @pending_stock_h = Product.pending_stock(@products.pluck("id"))
-=======
     @staff, customers, @search_text = customer_param_filter(params, session[:user_id])
     @customers = customers.order_by_name.page(params[:page])
 
@@ -80,14 +41,10 @@ class ModelsController < ApplicationController
     @product_sub_type = results_val[1]
     products = results_val[2]
     @search_text = results_val[3]
->>>>>>> efafc87de3efa0844d5de64d6f57e3d3288f8a05
 
     @pending_stock_h = Product.pending_stock(products.pluck("id"))
     @products = products.order_by_name.page(params[:page])
 
-<<<<<<< HEAD
-=======
   end
 
->>>>>>> efafc87de3efa0844d5de64d6f57e3d3288f8a05
 end
--- bigcommerce/app/models/staff.rb
+++ bigcommerce/app/models/staff.rb
@@ -15,29 +15,10 @@ class Staff < ActiveRecord::Base
 	# Create a new user
 	# Staff.create(name: , email: , password: , password_confirmation: )
 
-<<<<<<< HEAD
 	def self.active_sales_staff
 	  where('active = 1 and user_type LIKE "Sales%"')
 	end
 
-=======
-	def csv_import
-	  CSV.foreach("db/csv/staff.csv", headers: true) do |row|
-			row_hash = row.to_hash
-			Staff.create(id: row_hash['id'], firstname: row_hash['FirstName'], lastname: row_hash['LastName'],\
-				nickname: row_hash['NickName'], email: row_hash['Email'], logon_details: row_hash['LogonDetails'],\
-				contact_number: row_hash['Mobile'], state: row_hash['State'], country: row_hash['Country'],\
-				user_type: row_hash['UserType'], display_report: row_hash['DisplayReport'],\
-				invoice_rights: row_hash['InvoiceRights'], product_rights: row_hash['ProductRights'],\
-				payment_rights: row_hash['PaymentRights'], active: row_hash['Active'])
-		end
-	end
-
-	def self.active_sales_staff
-	  where('active = 1 and user_type LIKE "Sales%"')
-	end
-
->>>>>>> efafc87de3efa0844d5de64d6f57e3d3288f8a05
 	def self.filter_by_id(staff_id)
 	  return find(staff_id)
 	end
