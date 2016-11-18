db5580c19a226d27baa004f04cd328694587d5f9
Hinam <mehrahinam@gmail.com>
2016-11-18 12:27:13 +1100

    xero revision end and start time update functions

--- bigcommerce/app/models/xero_revision.rb
+++ bigcommerce/app/models/xero_revision.rb
@@ -1,14 +1,14 @@
 class XeroRevision < ActiveRecord::Base
 
-	def self.end_update(end_time)
+	def self.end_update(start_time, end_time)
 		only_row = first
+		only_row.start_time = start_time.utc
 		only_row.end_time = end_time.utc
 		only_row.save
 	end
 
-	def self.start_update(start_time)
+	def self.start_update
 		only_row = first
-		only_row.start_time = start_time.utc
 		only_row.end_time = nil
 		only_row.save
 	end
