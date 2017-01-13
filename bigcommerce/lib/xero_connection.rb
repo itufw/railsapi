0aa495b3a231d2ead596af4619bca1c1e6d42262
Hinam <mehrahinam@gmail.com>
2017-01-13 11:33:39 +1100

    changed xero connection keys

--- bigcommerce/lib/xero_connection.rb
+++ bigcommerce/lib/xero_connection.rb
@@ -5,7 +5,7 @@ class XeroConnection
 
 	def connect
 
-		client = Xeroizer::PrivateApplication.new('YLOXMTHAIZFN3UOE5C2DTG4HHEPKD7', '9FQENZIBR4STELO2PM1PD4ZUM3UIMP', "config/privatekey.pem", :unitdp => 4)
+		client = Xeroizer::PrivateApplication.new('PJH08HPMIWBH4YBX2SDYVE7WDWX7BX', 'PPSNCIHM5WSAUDTHDVH4P6ALDYZHCT', "config/privatekey.pem", :unitdp => 4)
 		return client
 
 	end
