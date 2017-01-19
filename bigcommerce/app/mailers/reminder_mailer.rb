ce9103bd4114b90d5f8e4dc22d856120a23bc934
Lubard <lubard@hotmail.com>
2017-01-19 17:05:54 +1100

    modified:   app/mailers/reminder_mailer.rb 	modified:   app/views/pdf/pdf_invoice.pdf.erb 	modified:   config/schedule.rb fixed the bugs of pdf

--- bigcommerce/app/mailers/reminder_mailer.rb
+++ bigcommerce/app/mailers/reminder_mailer.rb
@@ -1,5 +1,7 @@
 class ReminderMailer < ActionMailer::Base
   require 'mail'
+  require 'wicked_pdf'
+
   default from: 'it@untappedwines.com'
   layout "mailer"
 
@@ -22,16 +24,28 @@ class ReminderMailer < ActionMailer::Base
     @selected_invoices.each do |i|
       invoice = XeroInvoice.includes(:xero_contact, :xero_invoice_line_items, :order).where("xero_invoices.invoice_number = #{i}").first
       bill_address = Address.where(id: invoice.invoice_number).count == 0 ? "missing" : Address.find(invoice.invoice_number)
-      customer_notes = Order.where("orders.id = '#{invoice.invoice_number}'").count == 0 ? "missing" : Order.find("#{invoice.invoice_number}").customer_notes
-      # params = { :invoice => invoice }
+      order = Order.includes(:order_products).where("orders.id = '#{invoice.invoice_number}'").count == 0 ? "missing" : Order.find("#{invoice.invoice_number}")
+      customer_notes = ((("missing".eql? order)==true) || ("".eql? order.customer_notes)) ? "missing" : order.customer_notes
+
+      # The Customer Type!
       # cust_type_id: 1.Retail, 2. Wholesale, 3. Staff
       if 2 == Customer.where("customers.xero_contact_id = '#{invoice.xero_contact_id}'").first.cust_type_id
         attachments["invoice\##{invoice.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(
-          render_to_string(:template => 'pdf/pdf_invoice', :locals => { :invoice => invoice, :bill_address => bill_address, :customer_notes => customer_notes})
+          render_to_string(
+              :template => 'pdf/pdf_invoice',
+              :locals => {:invoice => invoice,
+                          :bill_address => bill_address,
+                          :customer_notes => customer_notes,
+                          :order => order}
+            #  :footer =>{:html => {
+            #                       :template => 'pdf/footer.html.erb',
+            #                     },
+            #             :spacing => -65}
+             )
         )
       else
         attachments["invoice\##{invoice.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(
-          render_to_string(:template => 'pdf/pdf_invoice_retail', :locals => { :invoice => invoice, :bill_address => bill_address, :customer_notes => customer_notes})
+          render_to_string(:template => 'pdf/pdf_invoice_retail', :locals => { :invoice => invoice, :bill_address => bill_address, :customer_notes => customer_notes, :order => order})
         )
       end
     end
