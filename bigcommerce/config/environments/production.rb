42dbb791d5b939080559c5d4b47c4487a4e8f96d
Lubard <lubard@hotmail.com>
2017-01-19 14:58:36 +1100

    modified:   config/environments/production.rb config file change for mailer

--- bigcommerce/config/environments/production.rb
+++ bigcommerce/config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'untappedwines.com',
+    user_name:            'untappedfinewinesacc@gmail.com',
+    password:             'omega8992',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
