19ad3cca1645dcca2785078f597e3723c4595d3a
Lubard <lubard@hotmail.com>
2017-02-20 10:15:44 +1100

    changed lib/bigcommerce_connection

--- bigcommerce/lib/bigcommerce_connection.rb
+++ bigcommerce/lib/bigcommerce_connection.rb
@@ -4,7 +4,8 @@ class BigcommerceConnection
 	# Set them to ENV variables
 	Bigcommerce.configure do |config|
 		config.auth = 'legacy'
-		config.url = "https://www.untappedwines.com.au/api/v2/"
+		config.url = "https://store-35510.mybigcommerce.com/api/v2/"
+		# config.url = "https://www.untappedwines.com.au/api/v2/"
 		config.username = "hinam"
 		config.api_key = "9ad399ba11e26ff7d9806f17801d6135e48e1f94"
 	end
