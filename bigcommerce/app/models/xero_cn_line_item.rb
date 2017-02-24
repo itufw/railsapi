a7bec7d269e99b089c22ba05d79913ecdb2e12d4
Lubard <lubard@hotmail.com>
2017-02-24 15:10:33 +1100

    bug fixed for credit note line items model

--- bigcommerce/app/models/xero_cn_line_item.rb
+++ bigcommerce/app/models/xero_cn_line_item.rb
@@ -11,13 +11,13 @@ class XeroCNLineItem < ActiveRecord::Base
             time = Time.now.to_s(:db)
 
             next unless cn_line_item_doesnt_exist(li.line_item_id)
-                sql = "INSERT INTO xero_cn_line_items (xero_cn_line_item_id,\
-        				xero_credit_note_id, item_code, description, quantity, unit_amount, line_amount,\
-        				tax_amount, tax_type, account_code, created_at, updated_at) VALUES ('#{SecureRandom.uuid}',\
-        				'#{credit_note_id}', '#{li.item_code}', '#{li.description}', '#{li.quantity}', '#{li.unit_amount}',\
-        				'#{li.line_amount(true)}', '#{li.tax_amount}', '#{li.tax_type}',\
-        				'#{li.account_code}', '#{time}', '#{time}')"
-                ActiveRecord::Base.connection.execute(sq
+            sql = "INSERT INTO xero_cn_line_items (xero_cn_line_item_id,\
+    				xero_credit_note_id, item_code, description, quantity, unit_amount, line_amount,\
+    				tax_amount, tax_type, account_code, created_at, updated_at) VALUES ('#{SecureRandom.uuid}',\
+    				'#{credit_note_id}', '#{li.item_code}', '#{li.description}', '#{li.quantity}', '#{li.unit_amount}',\
+    				'#{li.line_amount(true)}', '#{li.tax_amount}', '#{li.tax_type}',\
+    				'#{li.account_code}', '#{time}', '#{time}')"
+            ActiveRecord::Base.connection.execute(sql)
         end
      end
 
