76ac97b2683735de2fc43c3aabdf03b29a781ea8
Lubard <lubard@hotmail.com>
2017-03-10 10:32:18 +1100

    contact person bug fixed

--- bigcommerce/app/models/xero_contact_person.rb
+++ bigcommerce/app/models/xero_contact_person.rb
@@ -17,12 +17,12 @@ class XeroContactPerson < ActiveRecord::Base
     unless contact_person_exists(person, contact_id)
       sql = "INSERT INTO xero_contact_people (xero_contact_id, customer_id, first_name, last_name,\
           email_address, include_in_emails, created_at, updated_at)\
-          VALUES ('#{contact_id}', '#{customer_id}', '#{person.first_name}', '#{person.last_name}',\
+          VALUES ('#{contact_id}', '#{customer_id}', \"#{person.first_name}\", \"#{person.last_name}\",\
           '#{person.email_address}', '#{is_include_in_mails}', '#{time}', '#{time}')"
     else
       sql = "UPDATE xero_contact_people SET email_address = '#{person.email_address}',\
       include_in_emails = '#{is_include_in_mails}', updated_at = '#{time}'\
-      WHERE xero_contact_id = '#{contact_id}' and first_name = '#{person.first_name}' and last_name = '#{person.last_name}'"
+      WHERE xero_contact_id = '#{contact_id}' and first_name = \"#{person.first_name}\" and last_name = \"#{person.last_name}\""
     end
     ActiveRecord::Base.connection.execute(sql)
   end
