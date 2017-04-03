c67759272f02b2d508b7b5ffdca795b0d777d357
Lubard <lubard@hotmail.com>
2017-04-03 16:11:54 +1000

    new filter for map

--- bigcommerce/app/helpers/calendar_helper.rb
+++ bigcommerce/app/helpers/calendar_helper.rb
@@ -1,4 +1,9 @@
 module CalendarHelper
+  def sales_last_order(params)
+    return "Sales" if ((params["filter_selector"].nil?) || ("".eql?params["filter_selector"]) ||  ("Sales".eql?params["filter_selector"]))
+    return "Last_Order"
+  end
+
   def map_filter(params, colour_guide, colour_range_origin)
     cust_type = params[:selected_cust_style] || []
     selected_staff = params[:selected_staff] || []
@@ -52,6 +57,34 @@ module CalendarHelper
       map_pin
   end
 
+  def customer_last_order_filter(params, colour_guide, colour_range_origin)
+    selected_cust_style, selected_staff, colour_range = map_filter(params, colour_guide, colour_range_origin)
+    start_date = date_check_map(params["start_time"], 1)
+    end_date = date_check_map(params["due_time"], 0)
+
+    customers = Customer.joins(:addresses).select("addresses.lat, addresses.lng, customers.*").where("addresses.lat IS NOT NULL").group("customers.id")
+    customers = customers.joins(:orders).select("orders.date_created as last_order_date, customers.id").order("orders.date_created DESC").group("customers.id")
+    customers = customers.select{|x| selected_cust_style.include? x.cust_style_id.to_s } unless selected_cust_style.blank?
+    customers = customers.select{|x| selected_staff.include? x.staff_id.to_s } unless selected_staff.blank?
+
+    customer_map = {}
+    start_date = (Date.parse start_date)
+
+    customers.each do |customer|
+      days_gap = (customer.last_order_date.to_date - start_date).to_i
+      days_gap = 0 if days_gap < 0
+      colour_range.keys.each do |colour|
+        if days_gap.between?(colour_range["#{colour}"].min, colour_range["#{colour}"].max)
+          customer_map[customer.id] = add_map_pin(colour, customer)
+          break
+        end #end if
+      end #end colour guide for loop
+    end #end customers for loop
+
+    staff = Staff.filter_by_ids(customers.map{|x| x.staff_id}.uniq)
+    [customer_map, staff]
+  end
+
 
   def customer_filter(params, colour_guide, colour_range_origin)
     selected_cust_style, selected_staff, colour_range = map_filter(params, colour_guide, colour_range_origin)
@@ -82,6 +115,20 @@ module CalendarHelper
     end #end customers for loop
 
     staff = Staff.filter_by_ids(customers.map{|x| x.staff_id}.uniq)
-    [customer_map, staff, start_date, end_date]
+    [customer_map, staff]
+  end
+
+  def hash_map_pins(customer_map)
+    hash = Gmaps4rails.build_markers(customer_map.keys()) do |customer_id, marker|
+      marker.lat customer_map[customer_id]["lat"]
+      marker.lng customer_map[customer_id]["lng"]
+      marker.picture({
+                        :url    => view_context.image_path(customer_map[customer_id]["url"]),
+                        :width  => 30,
+                        :height => 30
+                       })
+            marker.infowindow "<a href=\"http://188.166.243.138/customer/summary?customer_id=#{customer_id}&customer_name=#{customer_map[customer_id]["name"]}\">#{customer_map[customer_id]["name"]}</a>"
+      end
+    hash
   end
 end
