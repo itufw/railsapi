f2c36748d589e60a096178826a682c563b41c50c
Lubard <lubard@hotmail.com>
2017-06-08 14:16:51 +1000

    lead page update

--- bigcommerce/app/models/customer_lead.rb
+++ bigcommerce/app/models/customer_lead.rb
@@ -16,7 +16,7 @@ class CustomerLead < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 
-  scoped_search on: [:firstname, :lastname, :company, :actual_name, :address]
+  scoped_search on: %i[firstname lastname company actual_name address]
 
   self.per_page = 15
 
@@ -36,6 +36,12 @@ class CustomerLead < ActiveRecord::Base
     where("customer_leads.actual_name LIKE \"%#{name}%\"")
   end
 
+  def self.staff_change(staff_id, lead_id)
+    lead = filter_by_id(lead_id)
+    lead.staff_id = staff_id.to_i
+    lead.save
+  end
+
   def self.filter_staff(staff_id = nil)
     return all if staff_id.nil?
     where('customer_leads.staff_id = ?', staff_id)
@@ -52,6 +58,10 @@ class CustomerLead < ActiveRecord::Base
 
   def self.order_by_name(direction)
     order('actual_name ' + direction)
+  end
+
+  def self.order_by_style(direction)
+    includes(:cust_style).order('cust_style.name ' + direction)
   end
 
   def self.order_by_staff(direction)
