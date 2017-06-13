d9d730ae23183fd090fe8b878bfdd2d1a438379a
Lubard <lubard@hotmail.com>
2017-06-13 12:03:33 +1000

    cut the customer list for event check

--- bigcommerce/app/controllers/customer_controller.rb
+++ bigcommerce/app/controllers/customer_controller.rb
@@ -56,7 +56,7 @@ class CustomerController < ApplicationController
     display_orders(params, Order.customer_filter([@customer_id]))
 
     # task section
-    @activity = Task.joins(:staff).customer_tasks(@customer_id).expired?.order_by_id('DESC')
+    @activity = Task.joins(:staff).customer_tasks(@customer_id).order_by_id('DESC')
     @subjects = TaskSubject.filter_by_ids(@activity.map(&:subject_1).compact)
     # -------------------
     # multiple contact people
