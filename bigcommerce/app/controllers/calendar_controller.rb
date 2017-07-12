853e418eee9681a6d4339a2b23ca8e2b0e5de4d7
Lubard <lubard@hotmail.com>
2017-07-12 16:29:26 +1000

    Sync Address update

--- bigcommerce/app/controllers/calendar_controller.rb
+++ bigcommerce/app/controllers/calendar_controller.rb
@@ -1,6 +1,8 @@
 require 'calendar_helper.rb'
 require 'models_filter.rb'
 require 'fuzzystringmatch'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
 
 class CalendarController < ApplicationController
   skip_before_action :verify_authenticity_token
@@ -10,6 +12,28 @@ class CalendarController < ApplicationController
 
   include CalendarHelper
   include ModelsFilter
+
+  def cron_google_calendar
+    # OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+    #
+    # scope = 'https://www.googleapis.com/auth/calendar'
+    # uri = Rails.env.development? ? 'http://localhost:3000/callback' : 'http://cms.untappedwines.com.au/callback'
+    #
+    # client_id = Google::Auth::ClientId.from_file('/path/to/client_secrets.json')
+    # token_store = Google::Auth::Stores::FileTokenStore.new(
+    #   :file => '/path/to/tokens.yaml')
+    # authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+    #
+    # credentials = authorizer.get_credentials(user_id)
+    # if credentials.nil?
+    #   url = authorizer.get_authorization_url(base_url: OOB_URI )
+    #   puts "Open #{url} in your browser and enter the resulting code:"
+    #   code = gets
+    #   credentials = authorizer.get_and_store_credentials_from_code(
+    #     user_id: user_id, code: code, base_url: OOB_URI)
+    # end
+    # OK to use credentials
+  end
 
   def redirect
     uri = Rails.env.development? ? 'http://localhost:3000/callback' : 'http://cms.untappedwines.com.au/callback'
