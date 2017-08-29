a72ea2431ae5d0e227a94b00e8fdf410c0ffe368
Lubard <lubard@hotmail.com>
2017-08-29 12:42:45 +1000

    Auth check for status changes, introduce shortcut for order edition

--- bigcommerce/lib/order_status.rb
+++ bigcommerce/lib/order_status.rb
@@ -27,9 +27,7 @@ module OrderStatus
       Order.where(id: selected_orders).update_all(status_id: 25, last_updated_by: user_id)
       return ['Group Update', '']
     when 'Shipped'
-      Order.where(id: selected_orders).each do |order|
-        order.update_attributes({status_id: 2, last_updated_by: user_id})
-      end
+      Order.where(id: selected_orders).map{|x| x.update_attributes({status_id: 2, last_updated_by: user_id}) }
       return ['Group Update', '']
     when 'Approve'
       Order.find(params[:order_id]).update_attributes(order_params.merge({status_id: 9, last_updated_by: user_id}))
