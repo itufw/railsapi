30f10cd5af50d64771d8a57b2fcfd46f397c297b
Lubard <lubard@hotmail.com>
2017-09-26 15:10:17 +1000

    Warehouse Design -> Mobile side

--- bigcommerce/app/models/warehouse_examining.rb
+++ bigcommerce/app/models/warehouse_examining.rb
@@ -1,5 +1,6 @@
 class WarehouseExamining < ActiveRecord::Base
   belongs_to :product_no_ws
+  belongs_to :creator, class_name: :Staff, foreign_key: :count_staff_id
 
   def self.duplicated(product_id)
     where(product_no_ws_id: product_id, authorised_date: nil)
