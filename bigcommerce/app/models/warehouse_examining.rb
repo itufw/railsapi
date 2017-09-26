918b5066093947cb88c694351e388700ec74e0c9
Lubard <lubard@hotmail.com>
2017-09-26 12:05:43 +1000

    Warehouse Design / Customer Map Update

--- bigcommerce/app/models/warehouse_examining.rb
+++ bigcommerce/app/models/warehouse_examining.rb
@@ -2,10 +2,11 @@ class WarehouseExamining < ActiveRecord::Base
   belongs_to :product_no_ws
 
   def self.duplicated(product_id)
-    where(product_no_ws_id: product_id)
+    where(product_no_ws_id: product_id, authorised_date: nil)
   end
 
   def self.pending
     where(authorised_date: nil)
   end
+
 end
