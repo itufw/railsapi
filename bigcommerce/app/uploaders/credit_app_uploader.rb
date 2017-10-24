6dde70f97e0f5259ad20b3375c0becb1f8d1b84a
Lubard <lubard@hotmail.com>
2017-10-24 15:14:16 +1100

    Dropbox GEM updated

--- bigcommerce/app/uploaders/credit_app_uploader.rb
+++ bigcommerce/app/uploaders/credit_app_uploader.rb
@@ -13,7 +13,7 @@ class CreditAppUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "CreditApplication/#{model.trading_name}"
+    "/CreditApplication/#{model.trading_name}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
