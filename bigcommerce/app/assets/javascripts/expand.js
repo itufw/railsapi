675c4f32f9a9920c77d20f02ead41f8c15b0a831
charveasna <charveasna@hotmail.com>
2018-08-05 02:15:02 +1000

    update and redesign orders status page

--- bigcommerce/app/assets/javascripts/expand.js
+++ bigcommerce/app/assets/javascripts/expand.js
@@ -1,22 +1,22 @@
-$(document.body).ready(
-    function () {
-        var change = function () {
-            $("#products-view-button").click(function() {
-				$(".orders-view").removeClass("default")
-				$(".orders-view").addClass("not-default")
+// $(document.body).ready(
+//     function () {
+//         var change = function () {
+//             $("#products-view-button").click(function() {
+// 				$(".orders-view").removeClass("default")
+// 				$(".orders-view").addClass("not-default")
 
-				$(".products-view").removeClass("not-default")
-				$(".products-view").addClass("default")
-			});
+// 				$(".products-view").removeClass("not-default")
+// 				$(".products-view").addClass("default")
+// 			});
 
-			$("#orders-view-button").click(function() {
-				$(".orders-view").removeClass("not-default")
-				$(".orders-view").addClass("default")
+// 			$("#orders-view-button").click(function() {
+// 				$(".orders-view").removeClass("not-default")
+// 				$(".orders-view").addClass("default")
 
-				$(".products-view").removeClass("default")
-				$(".products-view").addClass("not-default")
-			});
-        }
-        change();  //call it when the page is loaded
-    }
-);
+// 				$(".products-view").removeClass("default")
+// 				$(".products-view").addClass("not-default")
+// 			});
+//         }
+//         change();  //call it when the page is loaded
+//     }
+// );
